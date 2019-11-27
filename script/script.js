405620166f76ea023ada039e147a08abd07f02f8
martingess <martingess@protonmail.ch>
2019-11-27 14:23:48 +0200

    refactoring

--- script/script.js
+++ script/script.js
@@ -37,19 +37,19 @@ playerNamesUpdate(playersNames);
 
 //Данные игроков
 class User {
-    constructor(name, influence, playersAddInfluenceInput, hpBarCurrent, btnSendResults, oldParam){
+    constructor(name, influence, playersAddInfluenceInput, hpBarCurrent, btnSendResults, oldParam) {
         this.name = name,
-        this.currentInfluence = influence,
-        this.addInfluence = playersAddInfluenceInput,
-        this.influenceBar = hpBarCurrent,
-        this.btnSendResults = btnSendResults,
-        this.oldParam = oldParam;
+            this.currentInfluence = influence,
+            this.addInfluence = playersAddInfluenceInput,
+            this.influenceBar = hpBarCurrent,
+            this.btnSendResults = btnSendResults,
+            this.oldParam = oldParam;
     }
 }
 
 let players = [];
 
-for (let i = 0; i<4; i++){
+for (let i = 0; i < 4; i++) {
     players[i] = new User(playersNames[i], influence[i], playersAddInfluenceInput[i], hpBarCurrent[i], sendResults[i])
 }
 
@@ -71,44 +71,22 @@ players[3].currentInfluence.value = startInfluence;
 
 
 //листнеры для кнопок подсчёта
-players[0].btnSendResults.addEventListener('click', (event) => {
-    event.preventDefault();
-    makeResult(players[0]);
-});
-players[1].btnSendResults.addEventListener('click', (event) => {
-    event.preventDefault();
-    makeResult(players[1])
-});
-players[2].btnSendResults.addEventListener('click', (event) => {
-    event.preventDefault();
-    makeResult(players[2])
-});
-players[3].btnSendResults.addEventListener('click', (event) => {
-    event.preventDefault();
-    makeResult(players[3])
-});
+for (i = 0; i<4; i++){
+    players[i].btnSendResults.addEventListener('click', (event) => {
+        event.preventDefault();
+        makeResult(players[i]);
+    });
+}
 
 //вернуть старые результаты лиснтеры
-returnOldBtns[0].addEventListener("click", function (event) {
-    event.preventDefault();
-    returnOld(players[0]);
-    hpBarUpdate(players[0])
-})
-returnOldBtns[1].addEventListener("click", function (event) {
-    event.preventDefault();
-    returnOld(players[1]);
-    hpBarUpdate(players[1])
-})
-returnOldBtns[2].addEventListener("click", function (event) {
-    event.preventDefault();
-    returnOld(players[2]);
-    hpBarUpdate(players[2])
-})
-returnOldBtns[3].addEventListener("click", function (event) {
-    event.preventDefault();
-    returnOld(players[3]);
-    hpBarUpdate(players[3]);
-})
+for (i = 0; i<4; i++){
+    returnOldBtns[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        returnOld(players[i]);
+        hpBarUpdate(players[i])
+    })
+}
+
 
 function returnOld(player) {
     let tempOldParam = player.currentInfluence.value;
@@ -121,7 +99,7 @@ function makeResult(player) {
     if (!(isNaN(player.addInfluence.value))) {
         player.oldParam = +player.currentInfluence.value;
         player.currentInfluence.value = (+player.addInfluence.value) + (+player.currentInfluence.value);
-        hpBarUpdate(player)
+        hpBarUpdate(player);
         logUpdate("afterChange", player);
     }
     player.addInfluence.value = "";
@@ -142,25 +120,30 @@ function logUpdate(type, player) {
 }
 
 function hpBarUpdate(player) {
-    if (player === 'all'){
-
-    }
-
-    if ((player.currentInfluence.value / startInfluence) >= 1) {
-        player.influenceBar.style.background = "darkblue";
-    } else if ((player.currentInfluence.value / startInfluence) >= 0.7 && (player.currentInfluence.value / startInfluence) < 1) {
-        player.influenceBar.style.background = '#3aa56a';
-    } else if (player.currentInfluence.value / startInfluence < 0.7 && player.currentInfluence.value / startInfluence > 0.3) {
-        player.influenceBar.style.background = 'yellow';
+    if (player === 'all') {
+        for(let i = 0; i<4; i++){
+            barUpdate(players[i]);
+        }
     } else {
-        player.influenceBar.style.background = 'red';
-    }
-    if (player.currentInfluence.value > 0) {
-        player.influenceBar.style.width = (player.currentInfluence.value / startInfluence) * 100 + "%";
-    } else {
-        player.influenceBar.style.width = 0 + "%";
+        barUpdate(player);
     }
 
+    function barUpdate(player) {
+        if ((player.currentInfluence.value / startInfluence) >= 1) {
+            player.influenceBar.style.background = "darkblue";
+        } else if ((player.currentInfluence.value / startInfluence) >= 0.7 && (player.currentInfluence.value / startInfluence) < 1) {
+            player.influenceBar.style.background = '#3aa56a';
+        } else if (player.currentInfluence.value / startInfluence < 0.7 && player.currentInfluence.value / startInfluence > 0.3) {
+            player.influenceBar.style.background = 'yellow';
+        } else {
+            player.influenceBar.style.background = 'red';
+        }
+        if (player.currentInfluence.value > 0) {
+            player.influenceBar.style.width = (player.currentInfluence.value / startInfluence) * 100 + "%";
+        } else {
+            player.influenceBar.style.width = 0 + "%";
+        }
+    }
 }
 
 let save1 = JSON.parse(window.localStorage.getItem("save1"))
@@ -169,8 +152,8 @@ let save3 = JSON.parse(window.localStorage.getItem("save3"))
 
 // Проверка на сохранения
 
-function checkSave(save, slotNum){
-    if ( !(save) ){
+function checkSave(save, slotNum) {
+    if (!(save)) {
         savesSlots[slotNum].textContent = "Пустой слот";
     } else {
         let saveDate = new Date(save.nowDate)
@@ -183,28 +166,28 @@ checkSave(save2, 1);
 checkSave(save3, 2);
 
 
-class SavePlayerInfo{
-    constructor(){
-        this.playerOne = {
-            influence: players[0].currentInfluence.value,
-            name: players[0].name
-        },
-        this.playerTwo = {
-            influence: players[1].currentInfluence.value,
-            name: players[1].name
-        },
-        this.players[2] = {
-            influence: players[2].currentInfluence.value,
-            name: players[2].name
-        },
-        this.players[3] = {
-            influence: players[3].currentInfluence.value,
-            name: players[3].name
-        },
-        this.numberOfPlayers = numberOfPlayers,
-        this.startInfluence = startInfluence,
-        this.playersNames = playersNames,
-        this.nowDate = new Date();
+class SavePlayerInfo {
+    constructor() {
+        this.player1 = {
+                influence: players[0].currentInfluence.value,
+                name: players[0].name
+            },
+            this.player2 = {
+                influence: players[1].currentInfluence.value,
+                name: players[1].name
+            },
+            this.player3 = {
+                influence: players[2].currentInfluence.value,
+                name: players[2].name
+            },
+            this.player4 = {
+                influence: players[3].currentInfluence.value,
+                name: players[3].name
+            },
+            this.numberOfPlayers = numberOfPlayers,
+            this.startInfluence = startInfluence,
+            this.playersNames = playersNames,
+            this.nowDate = new Date();
     }
 }
 
@@ -227,70 +210,34 @@ saveBtn[2].addEventListener("click", (event) => {
     window.localStorage.setItem("save3", JSON.stringify(save1));
 });
 
-function loadSave(save){
+function loadSave(save) {
     startInfluence = save.startInfluence;
-    players[0].currentInfluence.value = save.players[0].influence;
-    players[1].currentInfluence.value = save.players[1].influence;
-    players[2].currentInfluence.value = save.players[2].influence;
-    players[3].currentInfluence.value = save.players[3].influence;
+    setNumberOfPlayers(+save.numberOfPlayers);
+    players[0].currentInfluence.value = save.player1.influence;
+    players[1].currentInfluence.value = save.player2.influence;
+    players[2].currentInfluence.value = save.player3.influence;
+    players[3].currentInfluence.value = save.player4.influence;
+    playerNamesUpdate(save.playersNames);
+    hpBarUpdate("all");
 }
 
 loadBtn[0].addEventListener("click", (event) => {
     event.preventDefault()
-    setNumberOfPlayers(+save1.numberOfPlayers);
-    startInfluence = save1.startInfluence;
-    players[0].currentInfluence.value = save1.players[0].influence;
-    players[1].currentInfluence.value = save1.players[1].influence;
-    players[2].currentInfluence.value = save1.players[2].influence;
-    players[3].currentInfluence.value = save1.players[3].influence;
-
-    playerNamesUpdate(save1.playersNames)
-
-    hpBarUpdate(players[0]);
-    hpBarUpdate(players[1])
-    hpBarUpdate(players[2])
-    hpBarUpdate(players[3])
-
+    loadSave(save1)
 })
 loadBtn[1].addEventListener("click", (event) => {
     event.preventDefault()
-    setNumberOfPlayers(+save2.numberOfPlayers);
-    startInfluence = save2.startInfluence;
-    players[0].currentInfluence.value = save2.players[0].influence;
-    players[1].currentInfluence.value = save2.players[1].influence;
-    players[2].currentInfluence.value = save2.players[2].influence;
-    players[3].currentInfluence.value = save2.players[3].influence;
-
-    playerNamesUpdate(save2.playersNames)
-
-    hpBarUpdate(players[0]);
-    hpBarUpdate(players[1])
-    hpBarUpdate(players[2])
-    hpBarUpdate(players[3])
-
+    loadSave(save2)
 })
 loadBtn[2].addEventListener("click", (event) => {
     event.preventDefault()
-    setNumberOfPlayers(+save3.numberOfPlayers);
-    startInfluence = save3.startInfluence;
-    players[0].currentInfluence.value = save3.players[0].influence;
-    players[1].currentInfluence.value = save3.players[1].influence;
-    players[2].currentInfluence.value = save3.players[2].influence;
-    players[3].currentInfluence.value = save3.players[3].influence;
-
-    playerNamesUpdate(save3.playersNames)
-
-    hpBarUpdate(players[0]);
-    hpBarUpdate(players[1])
-    hpBarUpdate(players[2])
-    hpBarUpdate(players[3])
-
+    loadSave(save3)
 })
 
-saveMenuBtn.addEventListener("click", ()=>{
+saveMenuBtn.addEventListener("click", () => {
     saveswrapper.style.display = "flex";
     saveswrapper.style.marginLeft = "0px";
 });
-closeSavesBtn.addEventListener("click", ()=>{
+closeSavesBtn.addEventListener("click", () => {
     saveswrapper.style.display = "none";
 })
